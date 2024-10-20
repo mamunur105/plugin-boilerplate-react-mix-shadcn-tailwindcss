c09e96a3beff65772a4a7d127fe624a6f37b961a
Mamunur Rashid <rmamunur105@gmail.com>
2024-10-20 21:09:11 +0600

    ..

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { Button } from 'shadcn-ui';
 
 const App = () => {
     return (
         <>
             <h1 className="text-3xl font-bold underline">
-                Hello world!
+                <Button variant="outline">Button</Button>
             </h1>
         </>
     );
